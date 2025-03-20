f757a8a5ff74bd731790a49bc700fa759c16528b
salmanrishu817 <116068084+salmanrishu817@users.noreply.github.com>
2025-03-20 12:43:38 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+export default {
+  build: {
+    rollupOptions: {
+      external: ['/src/main.tsx']
+    }
+  }
+}
